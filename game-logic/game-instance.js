5069a15f828c315e0ed196c3b64b84cf40c1d2ec
Adrian-Jablonski <Jablonski.Adrian1@gmail.com>
2019-07-29 20:36:47 -0500

    Added mocha --watch to automatically watch all tests

---
+++ game-logic/game-instance.js
@@ -0,0 +1,8 @@
+// The following watch command is used to watch this function for any changes and will run the test right away
+// mocha --watch ./test/game-test.js ./game-logic/game-instance.js
+
+function checkGameStatus(player) {
+    return false;
+}
+
+module.exports.checkGameStatus = checkGameStatus;
--- test/game-test.js
+++ test/game-test.js
@@ -1,7 +1,25 @@
 var expect = require("chai").expect;
 
 describe("GAME INSTANCE FUNCTIONS", function() {
+    describe("pending test example", function() {
+        it("This is a pending test"); // This is considered a pending test since it does not have a function
+    });
     describe("checkGameStatus", function() {
-        it("should tell me when the game is over"); // This is considered a pending test since it does not have a function
+        var checkGameStatus = require("../game-logic/game-instance.js")
+            .checkGameStatus;
+        it("should tell me when the game is over", function() {
+            var players = [
+                {
+                    ships: [
+                        {
+                            locations: [[0, 0]],
+                            damage: [[0, 0]]
+                        }
+                    ]
+                }
+            ];
+            var actual = checkGameStatus(players);
+            expect(actual).to.be.false;
+        });
     });
 });
